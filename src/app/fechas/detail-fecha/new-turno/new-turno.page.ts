c49a4a125f6fb1f19b53466092cb5f73e4ef1d87
Fardito <suarezfed@gmail.com>
2020-10-13 17:49:06 -0300

    turn form added

--- src/app/fechas/detail-fecha/new-turno/new-turno.page.ts
+++ src/app/fechas/detail-fecha/new-turno/new-turno.page.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { NavController } from '@ionic/angular';
+import { Subscription } from 'rxjs';
+import { Cliente } from 'src/app/clientes/clientes.model';
+import { ClientesService } from 'src/app/clientes/clientes.service';
+import { Fecha } from '../../fechas.model';
+import { FechasService } from '../../fechas.service';
 
 @Component({
   selector: 'app-new-turno',
   templateUrl: './new-turno.page.html',
   styleUrls: ['./new-turno.page.scss'],
 })
-export class NewTurnoPage implements OnInit {
+export class NewTurnoPage implements OnInit, OnDestroy {
+  @ViewChild('f') form: NgForm;
 
-  constructor() { }
+  loadedClients: Cliente[];
+  clientSub: Subscription;
+  fechaSub: Subscription;
+  fechaActual: Fecha;
+
+  constructor(private clientService: ClientesService, private activeRoute: ActivatedRoute, private navCtrl: NavController, private fechaService: FechasService) { }
 
   ngOnInit() {
+    this.clientSub = this.clientService.clientes.subscribe( clientes => {
+      this.loadedClients = clientes;
+    });
+
+    this.activeRoute.paramMap.subscribe((paramMap) => {
+      if (!paramMap.has("fechaId")) {
+        this.navCtrl.navigateBack("/fechas");
+        return;
+      }
+      this.fechaSub = this.fechaService
+        .getFecha(paramMap.get("fechaId"))
+        .subscribe((fecha) => {
+          this.fechaActual = fecha;
+        });
+    });
+  }
+
+  onAddTurno(){
+    console.log('Adding Turno...');
+    this.navCtrl.navigateBack(`/fechas/detail-fecha/${this.fechaActual.id}`);
+  }
+
+  ngOnDestroy(){
+    if(this.clientSub){
+      this.clientSub.unsubscribe();
+    }
+    if(this.fechaSub){
+      this.fechaSub.unsubscribe();
+    }
   }
 
 }
